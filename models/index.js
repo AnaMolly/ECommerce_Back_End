fdfe85dc48c028e6b9fa6399307661aeb2b0cd74
AnaMolly <analia.mohamdally@gmail.com>
2021-06-24 19:11:11 -0400

    added to the model files. edited server code to connect to sql. added a get route for all products. added gitignore file

--- models/index.js
+++ models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
+
+Product.belongsToMany(Tag, {
+  through:{
+      model: ProductTag
+  },as:'products_tags',
+  foreignKey: 'product_id'
+});
+
+
+Tag.belongsToMany(Product, {
+  through:{
+      model: ProductTag
+  },as:'tags_products',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
