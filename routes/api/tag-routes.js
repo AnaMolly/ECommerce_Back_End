a407954e9a93c11a267413f6501a02f3062a93ab
AnaMolly <analia.mohamdally@gmail.com>
2021-06-24 21:52:38 -0400

    added delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -58,7 +58,15 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-  
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }})
+      res.status(200).json(tagData);
+  } catch (err) {
+    console.log(err)
+}
 });
 
 module.exports = router;
